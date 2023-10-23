f6f1d7a275c0eb3f56fd211a41694aef8cfb3d62
dvidirin <dvn.face@gmail.com>
2023-10-23 20:39:41 -0300

    update commit: adding CRUD

--- app.py
+++ app.py
@@ -1,40 +1,98 @@
-from flask import Flask, render_template
-
-
-# dicionário de personagens
-
-dicionario = {
-    1: {
-        "titulo": "Aventuras Arqueológicas",
-        "nome": "Dr. David Mitchell",
-        "ocupacao": "Renomado arqueólogo e historiador",
-        "descricao": "Professor titular da Universidade de Oxford e diretor do Instituto de Arqueologia da universidade. Suas principais interesses de pesquisa incluem a história antiga da Grécia e Roma, bem como a arqueologia da Mesopotâmia.",
-        "aparencia": "Homem alto e magro, cabelos grisalhos e olhos azuis penetrantes.",
-        "personalidade": "Acadêmico brilhante, apaixonado por seu trabalho, defensor da preservação do patrimônio cultural e um homem de grande compaixão e empatia.",
-        "imagem": "static\images\david\david_image.jpg",
-    },
-    2: {
-        "titulo": "Aventuras Arqueológicas",
-        "nome": "Maya",
-        "ocupacao": "Jovem arqueóloga ardente e determinada",
-        "descricao": "Nascida em uma pequena cidade no Brasil, estudou arqueologia na Universidade de São Paulo. Trabalhou em várias escavações de sítios arqueológicos antigos, incluindo ruínas de templos, cidades e vilas.",
-        "aparencia": "Mulher de cabelos pretos e olhos castanhos, com um sorriso que ilumina uma sala.",
-        "personalidade": "Apaixonada por seu trabalho, determinada a fazer contribuições significativas para o campo da arqueologia e acredita que a arqueologia pode nos ajudar a entender melhor o passado e a construir um futuro melhor.",
-        "imagem": "static\images\maya\maya_image.jpg",
-    },
-}
+from flask import Flask, render_template, request, redirect, url_for
+from dados import personagens, inimigos
 
 app = Flask(__name__)
 
 
+# Rota da página principal
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+# Rota da página de personagens
 @app.route("/personagens")
 def os_personagens():
-    return render_template("personagens.html", personagens=dicionario)
+    return render_template("personagens.html", personagens=personagens)
+
+
+# Rota para listar os inimigos
+@app.route("/inimigos")
+def listar_inimigos():
+    return render_template("inimigos.html", inimigos=inimigos)
+
+
+# Rota para editar um inimigo
+@app.route("/editar/<int:id>")
+def editar_inimigo(id):
+    if id in inimigos:
+        inimigo = inimigos[id]
+        return render_template("editar_inimigo.html", inimigo=inimigo)
+    return "Inimigo não encontrado."
+
+
+# Função para retornar um inimigo
+def retornar_inimigo(id: int):
+    if id in inimigos.keys():
+        return inimigos[id]
+    else:
+        return {}
+
+
+# Função para atualizar um inimigo
+def atualizar_inimigo(id: int, dados_inimigo: dict):
+    inimigos[id] = dados_inimigo
+
+
+# Rota para atualizar um inimigo após a edição
+@app.route("/atualizar_inimigo/<int:id>", methods=["POST"])
+def atualizar_inimigo(inimigo_id):
+    if request.method == "POST":
+        if "salvar" in request.form:
+            inimigo = {}
+            inimigo["nome"] = request.form["nome"]
+            inimigo["tipo"] = request.form["tipo"]
+            inimigo["poder"] = request.form["poder"]
+            inimigo["vida"] = request.form["vida"]
+
+            if id in retornar_inimigo.keys():
+                atualizar_inimigo(id, inimigo)
+
+            # Redirecione de volta para a página de inimigos
+            return redirect(url_for("inimigos"))
+    else:
+        inimigo = retornar_inimigo(id)
+        inimigo["id"] = id
+        return render_template("editar_inimigo.html", **inimigo)
+
+
+# Rota para deletar um inimigo
+@app.route("/deletar/<int:id>")
+def deletar_inimigo(id):
+    if id in inimigos:
+        del inimigos[id]
+        return redirect("/inimigos")
+    return "Inimigo não encontrado."
+
+
+# Rota para adicionar um novo inimigo
+@app.route("/adicionar")
+def adicionar_inimigo():
+    return render_template("adicionar_inimigo.html")
+
+
+# Rota para criar um novo inimigo
+@app.route("/criar_inimigo", methods=["POST"])
+def criar_inimigo():
+    novo_id = max(inimigos.keys()) + 1
+    novo_inimigo = {
+        "nome": request.form["nome"],
+        "tipo": request.form["tipo"],
+        "poder": int(request.form["poder"]),
+        "vida": int(request.form["vida"]),
+    }
+    inimigos[novo_id] = novo_inimigo
+    return redirect("/inimigos")
 
 
 app.run(debug=True)
---
+++ dados.py
@@ -0,0 +1,51 @@
+personagens = {
+    1: {
+        "titulo": "Aventuras Arqueológicas",
+        "nome": "Dr. David Mitchell",
+        "ocupacao": "Renomado arqueólogo e historiador",
+        "descricao": "Professor titular da Universidade de Oxford e diretor do Instituto de Arqueologia da universidade. Suas principais interesses de pesquisa incluem a história antiga da Grécia e Roma, bem como a arqueologia da Mesopotâmia.",
+        "aparencia": "Homem alto e magro, cabelos grisalhos e olhos azuis penetrantes.",
+        "personalidade": "Acadêmico brilhante, apaixonado por seu trabalho, defensor da preservação do patrimônio cultural e um homem de grande compaixão e empatia.",
+        "imagem": "static\images\david\david_image.jpg",
+    },
+    2: {
+        "titulo": "Aventuras Arqueológicas",
+        "nome": "Maya",
+        "ocupacao": "Jovem arqueóloga ardente e determinada",
+        "descricao": "Nascida em uma pequena cidade no Brasil, estudou arqueologia na Universidade de São Paulo. Trabalhou em várias escavações de sítios arqueológicos antigos, incluindo ruínas de templos, cidades e vilas.",
+        "aparencia": "Mulher de cabelos pretos e olhos castanhos, com um sorriso que ilumina uma sala.",
+        "personalidade": "Apaixonada por seu trabalho, determinada a fazer contribuições significativas para o campo da arqueologia e acredita que a arqueologia pode nos ajudar a entender melhor o passado e a construir um futuro melhor.",
+        "imagem": "static\images\maya\maya_image.jpg",
+    },
+}
+
+inimigos = {
+    1: {
+        "nome": "Inimigo 1",
+        "tipo": "Zumbi",
+        "poder": 5,
+        "vida": 20,
+        "imagem": "static/images/zumbi/zumbi_image.jpg",
+    },
+    2: {
+        "nome": "Inimigo 2",
+        "tipo": "Esqueleto",
+        "poder": 8,
+        "vida": 15,
+        "imagem": "static/images/esqueleto/esqueleto_image.jpg",
+    },
+    3: {
+        "nome": "Inimigo 3",
+        "tipo": "Múmia",
+        "poder": 7,
+        "vida": 25,
+        "imagem": "static/images/mumia/mumia_image.jpg",
+    },
+    4: {
+        "nome": "Inimigo 4",
+        "tipo": "Fantasma",
+        "poder": 10,
+        "vida": 10,
+        "imagem": "static/images/fantasma/fantasma_image.jpg",
+    },
+}
