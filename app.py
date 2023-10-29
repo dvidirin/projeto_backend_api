7e5efb670596e78a961495ae3f6a5f7bdd861200
dvidirin <dvn.face@gmail.com>
2023-10-28 22:28:07 -0300

    update: add CRUD solution with BD sqlite

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
-from dados import personagens, inimigos
+import dados
+from dados import personagens
+import sqlite3
 
 app = Flask(__name__)
+
+
+# LEMBRE-SE ->
+# Ao obter dados do servidor, a máquina do cliente usa um GET
+# Ao enviar dados para o servidor, a máquina do cliente usa um POST
 
 
 # Rota da página principal
@@ -16,83 +23,107 @@ def os_personagens():
     return render_template("personagens.html", personagens=personagens)
 
 
-# Rota para listar os inimigos
+# Rota da página dos inimigos
 @app.route("/inimigos")
 def listar_inimigos():
-    return render_template("inimigos.html", inimigos=inimigos)
+    return render_template("inimigos.html", lista_inimigos=dados.retornar_inimigos())
 
 
-# Rota para editar um inimigo
-@app.route("/editar/<int:id>")
+# Abrir um inimigo em específico (carregando seus dados) no template editar_inimigo.html
+@app.route("/enemy/<int:id>", methods=["GET"])
+def exibir_inimigo(id):
+    id, nome, tipo, poder, vida, imagem = dados.retornar_inimigo(id)
+
+    return render_template(
+        "editar_inimigo.html",
+        id=id,
+        nome=nome,
+        tipo=tipo,
+        vida=vida,
+        poder=poder,
+        imagem=imagem,
+    )
+
+
+# NOVO com BD
+# usar essa rota para ATUALIZAR, EXCLUIR e CRIAR um novo inimigo.
+@app.route("/enemy/<int:id>", methods=["GET", "POST"])
 def editar_inimigo(id):
-    if id in inimigos:
-        inimigo = inimigos[id]
-        return render_template("editar_inimigo.html", inimigo=inimigo)
-    return "Inimigo não encontrado."
-
-
-# Função para retornar um inimigo
-def retornar_inimigo(id: int):
-    if id in inimigos.keys():
-        return inimigos[id]
-    else:
-        return {}
-
-
-# Função para atualizar um inimigo
-def atualizar_inimigo(id: int, dados_inimigo: dict):
-    inimigos[id] = dados_inimigo
-
-
-# Rota para atualizar um inimigo após a edição
-@app.route("/atualizar_inimigo/<int:id>", methods=["POST"])
-def atualizar_inimigo(inimigo_id):
     if request.method == "POST":
-        if "salvar" in request.form:
-            inimigo = {}
-            inimigo["nome"] = request.form["nome"]
-            inimigo["tipo"] = request.form["tipo"]
-            inimigo["poder"] = request.form["poder"]
-            inimigo["vida"] = request.form["vida"]
+        if "excluir" in request.form:
+            dados.remover_inimigo(id)
+            return redirect(url_for("listar_inimigos"))
+        elif "salvar" in request.form:
+            id = request.form["id"]
+            nome = request.form["nome"]
+            tipo = request.form["tipo"]
+            vida = request.form["vida"]
+            poder = request.form["poder"]
+            imagem = request.form["imagem"]
 
-            if id in retornar_inimigo.keys():
-                atualizar_inimigo(id, inimigo)
-
-            # Redirecione de volta para a página de inimigos
-            return redirect(url_for("inimigos"))
+            inimigo_retornado = dados.retornar_inimigo(id)
+            if inimigo_retornado:
+                dados.atualizar_inimigo(
+                    id=id, nome=nome, tipo=tipo, poder=poder, vida=vida, imagem=imagem
+                )
+            else:
+                dados.criar_inimigo(
+                    nome=nome, tipo=tipo, poder=poder, vida=vida, imagem=imagem
+                )
+            return redirect(url_for("listar_inimigos"))
     else:
-        inimigo = retornar_inimigo(id)
-        inimigo["id"] = id
-        return render_template("editar_inimigo.html", **inimigo)
+        # retorna os dados de um inimigo na página de editar_inimigo
+        id, nome, tipo, poder, vida, imagem = dados.retornar_inimigo(id)
+        return render_template(
+            "editar_inimigo.html",
+            id=id,
+            nome=nome,
+            tipo=tipo,
+            poder=poder,
+            vida=vida,
+            imagem=imagem,
+        )
 
 
-# Rota para deletar um inimigo
-@app.route("/deletar/<int:id>")
-def deletar_inimigo(id):
-    if id in inimigos:
-        del inimigos[id]
-        return redirect("/inimigos")
-    return "Inimigo não encontrado."
-
-
-# Rota para adicionar um novo inimigo
-@app.route("/adicionar")
-def adicionar_inimigo():
-    return render_template("adicionar_inimigo.html")
-
-
-# Rota para criar um novo inimigo
-@app.route("/criar_inimigo", methods=["POST"])
-def criar_inimigo():
-    novo_id = max(inimigos.keys()) + 1
-    novo_inimigo = {
-        "nome": request.form["nome"],
-        "tipo": request.form["tipo"],
-        "poder": int(request.form["poder"]),
-        "vida": int(request.form["vida"]),
-    }
-    inimigos[novo_id] = novo_inimigo
-    return redirect("/inimigos")
+# ANTIGO com DICIONARIO
+# Abrir o template editar_inimigo.html apenas com o id preenchido para permitir novo cadastro
+# Dar função aos botões excluir e salvar no template editar_inimigo.html
+# @app.route("/enemy/<int:id>", methods=["POST"])
+# a linha acima indica que a mesma rota que já havíamos criado antes foi recriada
+# #com o método POST, ou seja, quando o usuário enviar dados
+# def editar_inimigo(id):
+#    if "excluir" in request.form:
+# Aqui estamos verificando que "excluir" está contido na requisição, ou seja
+# o usuário clicou no botão excluir do formulário
+#        dados.remover_inimigo(id)
+#    elif "salvar" in request.form:
+# Aqui estamos verificando que "salvar" está contido na requisição, ou seja,
+# o usuário clicou em "salvar"
+# Criando um dicionário vazio para conter os dados do inimigo que será salvo
+#        inimigo = {}
+# colocamos no dicionário o conteúdo que veio do formulário
+#        inimigo["nome"] = request.form["nome"]
+#        inimigo["tipo"] = request.form["tipo"]
+#        inimigo["vida"] = request.form["vida"]
+#        inimigo["poder"] = request.form["poder"]
+#        inimigo["imagem"] = request.form["imagem"]
+# precisa definir se vai SALVAR um novo inimigo ou ATUALIZAR um inimigo já existente
+# inimigo_existente = dados.retornar_inimigo(id)
+#        inimigos = dados.retornar_inimigos()
+#        if id in inimigos.keys():
+#            dados.atualizar_inimigo(id, inimigo)
+#        else:
+#            dados.criar_inimigo(**inimigo)
+# vamos testar se o id do inimigo está no dicionário que contém todos eles.
+# if inimigo_existente[1]:
+#    inimigo["id"] = id
+# caso o id já exista, vamos chamar a função atualizar_inimigo, indicando o id e os novos dados
+#    dados.atualizar_inimigo(**inimigo)
+# else:
+# caso a id não exista, vamos chamar a função criar_inimigo passando os dados do dicionário
+#    dados.criar_inimigo(**inimigo)
+# o nosso return está fora dos ifs porque será executado INDEPENDENTEMENTE do botão que for clicado
+#    return redirect(url_for("listar_inimigos"))
 
 
 app.run(debug=True)
--- dados.py
+++ dados.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 personagens = {
     1: {
         "titulo": "Aventuras Arqueológicas",
@@ -18,34 +20,167 @@ personagens = {
         "imagem": "static\images\maya\maya_image.jpg",
     },
 }
-
+"""
 inimigos = {
     1: {
         "nome": "Inimigo 1",
         "tipo": "Zumbi",
         "poder": 5,
         "vida": 20,
-        "imagem": "static/images/zumbi/zumbi_image.jpg",
+        "imagem": "zumbi_image.jpg",
     },
     2: {
         "nome": "Inimigo 2",
         "tipo": "Esqueleto",
         "poder": 8,
         "vida": 15,
-        "imagem": "static/images/esqueleto/esqueleto_image.jpg",
+        "imagem": "esqueleto_image.jpg",
     },
     3: {
         "nome": "Inimigo 3",
         "tipo": "Múmia",
         "poder": 7,
         "vida": 25,
-        "imagem": "static/images/mumia/mumia_image.jpg",
+        "imagem": "mumia_image.jpg",
     },
     4: {
         "nome": "Inimigo 4",
         "tipo": "Fantasma",
         "poder": 10,
         "vida": 10,
-        "imagem": "static/images/fantasma/fantasma_image.jpg",
+        "imagem": "fantasma_image.jpg",
     },
 }
+"""
+
+# id generator antigo
+# def gerar_id():
+#    id = max(inimigos.keys()) + 1
+#    return id
+
+
+# id generator novo
+def gerar_id():
+    conn = sqlite3.connect("inimigos.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT seq FROM sqlite_sequence WHERE name='inimigo'")
+    next_id = cursor.fetchone()[0]
+    return next_id + 1
+
+
+# create inimigo antigo
+# def criar_inimigo(nome: str, tipo: str, poder: int, vida: int, imagem: str):
+#    inimigos[gerar_id()] = {
+#        "nome": nome,
+#        "tipo": tipo,
+#        "poder": poder,
+#        "vida": vida,
+#        "imagem": imagem,
+#    }
+
+
+# create inimigo novo
+def criar_inimigo(nome: str, tipo: str, poder: int, vida: int, imagem: str):
+    try:
+        conn = sqlite3.connect("inimigos.db")
+        cursor = conn.cursor()
+        sql_insert = "INSERT INTO inimigo (nome_inimigo, tipo_inimigo, poder_inimigo, vida_inimigo, imagem_inimigo) VALUES (?, ?, ?, ?, ?)"
+        cursor.execute(sql_insert, (nome, tipo, poder, vida, imagem))
+        id_inimigo = cursor.lastrowid
+        conn.commit()
+        conn.close()
+        return id_inimigo
+    except Exception as ex:
+        print(ex)
+        return 0
+
+
+# update antigo
+# def atualizar_inimigo(id: int, dados_inimigo: dict):
+#    inimigos[id] = dados_inimigo
+
+
+# update novo
+# atualiza os dados de um inimigo
+def atualizar_inimigo(id: int, nome, tipo, poder, vida, imagem):
+    try:
+        conn = sqlite3.connect("inimigos.db")
+        cursor = conn.cursor()
+        sql_update = "UPDATE inimigo SET nome_inimigo = ?, tipo_inimigo = ?, poder_inimigo = ?, vida_inimigo = ?, imagem_inimigo = ? WHERE id_inimigo = ?"
+        cursor.execute(sql_update, (nome, tipo, poder, vida, imagem, id))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+# delete antigo
+# def remover_inimigo(id: int):
+#    if id in inimigos.keys():
+#        del inimigos[id]
+
+
+# delete novo
+def remover_inimigo(id: int):
+    try:
+        conn = sqlite3.connect("inimigos.db")
+        cursor = conn.cursor()
+        sql_delete = "DELETE FROM inimigo WHERE id_inimigo = ?"
+        cursor.execute(sql_delete, (id,))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+# read antigo
+# def retornar_inimigo(id: int) -> dict:
+#    if id in inimigos.keys():
+#        return inimigos[id]
+#    else:
+#        return {}
+
+
+# read novo
+# retorna um único inimigo
+def retornar_inimigo(id: int):
+    try:
+        if id == 0:
+            return gerar_id(), "", "", "", "", ""
+        conn = sqlite3.connect("inimigos.db")
+        cursor = conn.cursor()
+        sql_select = "SELECT * FROM inimigo WHERE id_inimigo = ?"
+        cursor.execute(sql_select, (id,))
+        id, nome, tipo, poder, vida, imagem = cursor.fetchone()
+        conn.close()
+        return id, nome, tipo, poder, vida, imagem
+    except:
+        return False
+
+
+# read retorna todos os inimigos antigo
+# def retornar_inimigos() -> dict:
+#    return inimigos
+
+
+# read retorna todos os inimigos novo
+def retornar_inimigos():
+    try:
+        conn = sqlite3.connect("inimigos.db")
+        cursor = conn.cursor()
+        sql_select = "SELECT * FROM inimigo"
+        cursor.execute(sql_select)
+        inimigos = cursor.fetchall()
+        conn.close()
+        return inimigos
+    except:
+        return False
+
+
+# retorna os personagens principais
+def retornar_personagens() -> dict:
+    return personagens
